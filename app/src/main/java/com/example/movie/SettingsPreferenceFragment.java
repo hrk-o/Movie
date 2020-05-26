5815c564b91b304f5eddf351bd0ef384d32bda78
haruka <haruka@mznlabs-MacBook-Pro.local>
2020-05-26 15:09:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/movie/SettingsPreferenceFragment.java
@@ -0,0 +1,14 @@
+package com.example.movie;
+
+import android.app.Fragment;
+
+public class SettingsPreferenceFragment extends Fragment {
+
+
+    /**
+     *  Key for Preferences
+     */
+    public final static String KEY_AUDIO = "switch_audio";
+    public final static String KEY_STORAGE = "switch_storage";
+    public final static String KEY_FORMAT = "list_output_format";
+}
